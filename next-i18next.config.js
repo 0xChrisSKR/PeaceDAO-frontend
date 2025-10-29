74071eff028948b368df8ab37c8d60ed546dcb38
0xChris.SKR <liujiahao002@gmail.com>
2025-10-29 15:51:34 +0800

    Configure Next.js i18n setup

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+/** @type {import('next-i18next').UserConfig} */
+const i18nConfig = {
   i18n: {
-    defaultLocale: 'zh',
     locales: ['zh', 'en'],
-    localeDetection: false
+    defaultLocale: 'zh',
+    // Next.js 只接受 false；或移除此行。為了避免 Vercel 警告，設 false。
+    localeDetection: false,
   },
-  reloadOnPrerender: process.env.NODE_ENV === 'development'
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
+module.exports = i18nConfig;
