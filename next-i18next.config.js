16333616d8300d32ab94b92a0bc6c06735af335f
0xChris.SKR <liujiahao002@gmail.com>
2025-10-29 16:20:37 +0800

    Update i18n config and webpack aliases

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next-i18next').UserConfig} */
 const i18nConfig = {
   i18n: {
-    locales: ['zh', 'en'],
-    defaultLocale: 'zh',
+    locales: ['en', 'zh'],
+    defaultLocale: 'en',
     // Next.js 只接受 false；或移除此行。為了避免 Vercel 警告，設 false。
     localeDetection: false,
   },
