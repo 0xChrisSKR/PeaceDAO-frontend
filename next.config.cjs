54af5653ab5b1010293cebf05e05f841bd50c733
0xChris.SKR <liujiahao002@gmail.com>
2025-10-31 10:03:54 +0800

    Convert next.config.mjs to next.config.cjs format

--- next.config.cjs
+++ next.config.cjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: { esmExternals: 'loose' },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
-      "pino-pretty": false,
-      "pino-abstract-transport": false,
-      "sonic-boom": false,
-      "encoding": false
+      'pino-pretty': false,
+      'pino-abstract-transport': false,
+      'sonic-boom': false,
+      'encoding': false,
     };
     return config;
-  }
+  },
 };
 
 module.exports = nextConfig;
