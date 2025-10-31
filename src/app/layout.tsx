c536b6071d3a268ab4ac4ecf52f24a8a983673aa
0xChris.SKR <liujiahao002@gmail.com>
2025-10-31 12:41:48 +0800

    Refactor RootLayout to include Inter font and update description
    Updated layout to use Inter font and modified metadata description.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,20 @@
 // src/app/layout.tsx
 import './globals.css'
 import type { Metadata } from 'next'
-import { Web3Provider } from '@/providers/web3' // 這行要能對到 src/providers/web3.tsx
+import { Inter } from 'next/font/google'
+import { Web3Provider } from '@/providers/web3'
+
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'PeaceDAO',
-  description: 'Toward transparent fundraising & community governance',
+  description: 'World Peace DApp'
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={inter.className}>
         <Web3Provider>{children}</Web3Provider>
       </body>
     </html>
